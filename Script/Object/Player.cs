b1f750771119586b77d6960bc527d4a638d7aaa4
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-14 20:01:18 +0700

    architecture change

--- Script/Object/Player.cs
+++ Script/Object/Player.cs
@@ -1,31 +1,39 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody2D 
-{
+public class Player : KinematicBody2D {
+
 	public const float Speed = 20000.0f;
+
+	Vector2 Velocity = Vector2.Zero;
 	
 	private PistolGun gun = new PistolGun();
 
-	public override void _PhysicsProcess(double _delta) {
-		float delta = (float) _delta;
-		Vector2 velocity = Velocity;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta) {
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero) {
-			velocity.x = direction.x * Speed * delta;
-			velocity.y = direction.y * Speed * delta;
+			float monoDirection = 1.21f;
+			if (direction.x != 0 && direction.y != 0)
+				monoDirection = 1.0f;
+			Velocity.x = direction.x * Speed * monoDirection * delta;
+			Velocity.y = direction.y * Speed * monoDirection * delta;
 		} else {
-			velocity.x = Mathf.MoveToward(Velocity.x, 0, Speed);
-			velocity.y = Mathf.MoveToward(Velocity.y, 0, Speed);
+			Velocity.x = Mathf.MoveToward(Velocity.x, 0, Speed);
+			Velocity.y = Mathf.MoveToward(Velocity.y, 0, Speed);
 		}
 		
 		// The Gun
 		gun.Update(delta);
 
-		Velocity = velocity;
-		MoveAndSlide();
+		MoveAndSlide(Velocity);
 	}
 }
