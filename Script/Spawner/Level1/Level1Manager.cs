39489ea9d790f7ec9ee2e5672bade907fd67b76b
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-18 08:44:07 +0700

    input map

--- Script/Spawner/Level1/Level1Manager.cs
+++ Script/Spawner/Level1/Level1Manager.cs
@@ -6,13 +6,13 @@ public class Level1Manager : SpawnerManager {
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		AddPhase(new List<string>(){"WindFairySA-06-00", "WindFairySA-06-01"});
 		AddPhase(new List<string>(){"IceFairy-00-00", "IceFairy-00-01"});
 		AddPhase(new List<string>(){"FireFairy-01-00", "FireFairy-01-01"});
 		AddPhase(new List<string>(){"IceFairy-02-00", "FireFairy-02-01"});
 		AddPhase(new List<string>(){"FireFairy-03-00", "IceFairy-03-01"});
 		AddPhase(new List<string>(){"IceFairy-04-00"});
 		AddPhase(new List<string>(){"IceFairy-05-00", "WindFairy-05-01"});	// NOTE: WindFairy at 0.2 FireRate is SPLENDID
+		AddPhase(new List<string>(){"WindFairySA-06-00", "WindFairySA-06-01", "IceFairy-06-02"});
 		AddPhase(new List<string>(){"ClownpieceSpawner", "ClownpieceSupportSpawner-00", "ClownpieceSupportSpawner-01"});
 		base._Ready();
 	}
