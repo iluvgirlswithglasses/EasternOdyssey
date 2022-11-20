89fbc15e4b0062b7339c940823825692e8c371c5
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-20 18:23:19 +0700

    furry system

--- Script/Enemy/Yuuka.cs
+++ Script/Enemy/Yuuka.cs
@@ -3,13 +3,10 @@ using System;
 using static System.Math;
 
 public class Yuuka : VeloMovementEnemy {
-	
-	[Export]
-	public int Speed = 0;
-	[Export]
-	public float RotateSpeed = 1f;
 
 	private HealthDisplayer healthDisp;
+	private int phase = 0;
+	private int[] Thresholds = {900, 0};
 
 	public override void _Ready() {
 		base._Ready();
@@ -19,10 +16,7 @@ public class Yuuka : VeloMovementEnemy {
 				return new Vector2(-120f * (float) Cos(d/3), 0);
 			}
 			// the rest
-			return new Vector2(
-				0,
-				0
-			);
+			return new Vector2(0, 0);
 		};
 		
 		// healthbar
@@ -31,14 +25,34 @@ public class Yuuka : VeloMovementEnemy {
 		healthDisp.SetCurrentHealth(Health);
 		healthDisp.SetLabel("BOSS: ");
 		healthDisp.Visible = true;
+		
+		// phases
+		for (int i = 0; i < Thresholds.GetLength(0); i++) {
+			SetGunSetState(i, false);
+		}
+		SetGunSetState(0, true);
 	}
 
 	public override void TakeDamage(int d) {
 		base.TakeDamage(d);
 		healthDisp.SetCurrentHealth(Health);
+		if (Health <= 0)
+			return;
+		// 
+		if (Health <= Thresholds[phase]) {
+			SetGunSetState(phase, false);
+			phase++;
+			SetGunSetState(phase, true);
+		}
 	}
 
 	public override void ProcessOutOfScreen() {
 		// do nothing
+	}
+
+	protected void SetGunSetState(int i, bool state) {
+		foreach (Node2D node in GetNode<Node2D>(string.Format("GunSet{0}", i)).GetChildren()) {
+			node.SetProcess(state);
+		}
 	}
 }
