71aee82fa464aa1a5704cf2313d02d08281b3ca2
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-20 18:48:36 +0700

    background music bugfix

--- Script/UI/NextStageDisplayer.cs
+++ Script/UI/NextStageDisplayer.cs
@@ -7,6 +7,7 @@ public class NextStageDisplayer : Control {
 		if (PickupLevel.CurrentLevel == PickupLevel.FinalLevel) {
 			GD.Print("Ending Scene not implemented");
 		}
+		PickupMusic.D = 0;
 		GetTree().ChangeScene(string.Format("res://Asset/Level/Level{0}.tscn", PickupLevel.CurrentLevel + 1));
 	}
 }
