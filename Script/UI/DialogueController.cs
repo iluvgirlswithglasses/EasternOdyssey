722119fb64c37503daaf289a34660ebc82933152
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-11-22 09:21:22 +0700

    dialogue cutscene

---
+++ Script/UI/DialogueController.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class DialogueController : Control {
+
+	protected float Delta;
+
+	// the first dialogue is shown when scene starts
+	// so current dialogue is the second one
+	protected int CurrentDialogue = 1; 
+	List<Label> Dialogues;
+
+	protected Label Tutor;
+	protected Control Container;
+
+	public override void _Ready() {
+		Tutor = GetNode<Label>("Tutorial");
+		Container = GetNode<Control>("Container");
+
+		Dialogues = new List<Label>();
+		foreach (Label i in Container.GetChildren()) {
+			i.Visible = false;
+			Dialogues.Add(i);
+		}
+		Dialogues[0].Visible = true;
+
+		Tutor.SelfModulate = new Color(0, 0, 0, 0);
+	}
+
+	public override void _Process(float delta) {
+		// tutorial animation
+		Delta += delta;
+		Tutor.SelfModulate = new Color(1, 1, 1, (float) Math.Cos(Delta));
+		// dialogue
+		if (Input.IsActionJustPressed("next_dialogue")) {
+			if (CurrentDialogue == Dialogues.Count) {
+				// next level
+			} else {
+				Dialogues[CurrentDialogue++].Visible = true;
+			}
+		}
+	}
+}
